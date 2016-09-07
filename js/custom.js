717eee67bb094a291074c2a01ffbf23d1995547f
Zack Fischmann <zacharyrobert@gmail.com>
2016-09-06 20:22:46 -0500

    redesign / simplify website

--- js/custom.js
+++
@@ -1,5 +0,0 @@
-$( document ).ready(function() {
-    $("#floaterBig").fitText(1.0, { minFontSize: '45px', maxFontSize: '70px'});
-    $("#floaterSmall").fitText(1.5, { minFontSize: '30px', maxFontSize: '35px'});
-    $(".headline").fitText(1.5, { minFontSize: '30px', maxFontSize: '35px'});
-});
